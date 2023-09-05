0c37873181ea27fbe3d04ee8a3dd2d43f4f95c36
vengatesanb17 <121868457+vengatesanb17@users.noreply.github.com>
2023-09-05 18:05:12 +0530

    Create exclude-domains.js

---
+++ JS-file/exclude-domains.js
@@ -0,0 +1,32 @@
+// exclude-domains.js
+function scanNode(s) {
+    if (s.isInScope()) {
+        // Define an array of domains to exclude
+        var excludedDomains = [
+            "https://172.17.0.1/",
+            "https://example.com/",
+            // Add more domains to exclude as needed
+        ];
+
+        // Check if the URL of the scanned node matches any of the excluded domains
+        for (var i = 0; i < excludedDomains.length; i++) {
+            if (s.url.startsWith(excludedDomains[i])) {
+                s.setInScope(false);
+                break; // Stop checking after excluding one domain
+            }
+        }
+    }
+}
+
+function scan() {
+    var sites = [];
+    sites.push(OWASPZAP.getSiteByName("http://testphp.vulnweb.com/"));
+    if (sites.length > 0) {
+        for (var i = 0; i < sites.length; i++) {
+            var site = sites[i];
+            site.deleteAllUrls();
+            site.includeInContext("Default Context");
+            site.setIncludeInScope(true);
+        }
+    }
+}
